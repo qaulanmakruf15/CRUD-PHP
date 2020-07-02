06da02d45b51351613b10977f3a982861c2f1648
qaulanmakruf15 <38878442+qaulanmakruf15@users.noreply.github.com>
2020-07-02 22:18:54 +0700

    Update arkademy.sql

--- crud/arkademy.sql
+++ crud/arkademy.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 11 Nov 2019 pada 08.25
--- Versi server: 10.1.37-MariaDB
--- Versi PHP: 7.1.30
+-- Host: localhost
+-- Generation Time: Jul 02, 2020 at 05:18 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `produk`
+-- Table structure for table `produk`
 --
 
 CREATE TABLE `produk` (
@@ -33,38 +33,38 @@ CREATE TABLE `produk` (
   `nama_produk` varchar(255) DEFAULT NULL,
   `keterangan` text,
   `jumlah` int(11) DEFAULT NULL,
-  `harga` int(11) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `produk`
+-- Dumping data for table `produk`
 --
 
 INSERT INTO `produk` (`id`, `nama_produk`, `keterangan`, `jumlah`, `harga`) VALUES
-(1, 'Indomie', 'indomie ini enak sekali', 4000, 5000),
-(4, '342', '', 55, 234, '452-761-277-contoh.jpg'),
-(5, '123', '33', 55, 123, '623-452-761-277-contoh.jpg'),
-(6, '23', '44', 5, 234, '857-277-contoh.jpg');
+(1, 'Indomie', 'indomie ini enak sekali ', 4000, 5000),
+(4, 'Chitato', 'Cemilan ini enak sekali ', 55, 234),
+(5, 'sugus', 'Permen ini enak sekali', 55, 123),
+(6, 'Chocolatos', 'Coklat ini enak sekali', 5, 234);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `produk`
+-- Indexes for table `produk`
 --
 ALTER TABLE `produk`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `produk`
+-- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
